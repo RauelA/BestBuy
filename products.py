882dc3818f6ecc03a2f4aa6cded81e44e5768692
rauelon <rauel.anders@web.de>
2026-05-06 01:40:05 +0200

    ADDED: 'Product' class in products.py

---
+++ products.py
@@ -0,0 +1,49 @@
+
+
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}$, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        try:
+            self.quantity -= quantity
+            return float(self.price * quantity)
+        except (TypeError, ValueError) as e:
+            print(f"Error: {e}")
+            return False
+
+
+
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+bose.show()
+mac.show()
+
+bose.set_quantity(1000)
+bose.show()
